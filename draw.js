0be72eab596fa366328fb59a715162c927b9f991
haseaki0503 <haseaki0503@gmail.com>
2014-12-23 14:38:33 +0900

    動かせるようにしたよ。

--- draw.js
+++ draw.js
@@ -1,79 +1,56 @@
 //宣言
-// var player1 = player1 || {};
-// player1.posx = 250;
-// player1.posy = 250;
-// var player2 = player2 || {};
-// player2.posx = 250;
-// player2.posy = 250;
-
-var player = function(posx, posy) {
-		this.
-    this.posx = posx;
-    this.posy = posy;
+var player = function(posx, posy, canvas) {
+    this.posx         = posx;
+    this.posy         = posy;
+    this.canvas       = canvas;
 }
 
-var player1 = new player(250, 250);
-var player2 = new player(250, 250);
-
+//MAIN
 $(function(){
-	drawPlayer1();
-	drawPlayer2();
-	Control();
-	test2();
+	player1 = new player(250, 250, $('#player1').get(0));
+	player2 = new player(250, 250, $('#player2').get(0));
+
+	drawPlayer(player1.posx, player1.posy, player1.canvas);
+	drawPlayer(player2.posx, player2.posy, player2.canvas);
+	Controler();
 });
 
-function test2(){
-	console.log("positionTEST: " + player1.posx);
-}
 
-function drawPlayer1(){
-	//プレイヤー描画
-	var player = $('#player1').get(0);
-	if(!player || !player1.getContext){
+function drawPlayer(posx, posy, canvas){
+	if(!canvas || !canvas.getContext){
+		console.log("false");
 		return false;
 	};
 
-
-	var ctxPlayer1 = player.getContext('2d');
-	ctxPlayer1.beginPath();
-	ctxPlayer1.arc(player1.posx, player1.posy, 20, 0, Math.PI*2, false);
-	ctxPlayer1.stroke();
+	var ctxCanvas = canvas.getContext('2d');
+	ctxCanvas.clearRect(0,0,canvas.width,canvas.height);
+	ctxCanvas.beginPath();
+	ctxCanvas.arc(posx, posy, 20, 0, Math.PI*2, false);
+	ctxCanvas.stroke();
+	console.log(posx);
+	console.log(posy);
 };
 
 
-function drawPlayer2(){
-	//プレイヤー描画
-	var player2 = $('#player2').get(0);
-	if(!player2 || !player2.getContext){
-		return false;
-	};
-
-	var ctxPlayer2 = player2.getContext('2d');
-	ctxPlayer2.beginPath();
-	ctxPlayer2.arc(70, 70, 20, 0, Math.PI*2, false);
-	ctxPlayer2.stroke();
-};
-
-
-function Control(){
+function Controler(){
 	$(window).keyup(function(e){
 		//左が押されたとき
-		if(e.keyCode == 37){
-			player1.posx -= 1;
+		if(e.keyCode == 37 && player1.posx > 40){
+				player1.posx -= 30;
 		}
 		//右が押されたとき
-		if(e.keyCode == 39){
-			player1.posx += 1;
+		if(e.keyCode == 39 && player1.posx < 460){
+			player1.posx += 30;
 		}
 		//上が押されたとき
-		if(e.keyCode == 38){
-			player1.posy -= 1;
+		if(e.keyCode == 38 && player1.posy > 40){
+			player1.posy -= 30;
 		}
 		//下が押されたとき
-		if(e.keyCode == 40){
-			player1.posy += 1;
+		if(e.keyCode == 40 && player1.posy < 460){
+			player1.posy += 30;
 		}
-		// console.log("p1posx" + player1.posx);
-		// console.log("p1posy" + player1.posy);
+		//描画
+		drawPlayer(player1.posx, player1.posy, player1.canvas);
 	});
 };
