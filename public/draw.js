ab3d374fb823835ee81927a95602333121527f2a
daisyo <ngdsyma1@gmail.com>
2015-01-19 22:49:25 +0900

    update
    Extend

--- public/draw.js
+++ public/draw.js
@@ -1,21 +1,126 @@
-//宣言
+/****************************
+*    javascript draw.js 	*
+*    FPSと滑らかな移動の実装		*
+*****************************/
+
+
+// 全体で使用する変数を定義
+var canvas, ctx;	//	わかんない
+
+
+// FPS管理に使用するパラメータを定義
+const FPS = 30;			    	// １秒間の描画の回数
+const MSPF = 1000 / FPS;		// １フレームの時間
+var set_time;                   // 1フレーム開始時の時間
+var end_time;     　　　　　　　　　　　　// 1フレームにかかった時間
+var count = 0;					// フレーム回数
+// ...
+
+
+//	キー入力のフラグ
+var up_flag = 0;	//　↑ キーの入力フラグ
+var down_flag = 0;	//　↓ キーの入力フラグ
+var right_flag = 0;	//　→ キーの入力フラグ
+var left_flag = 0;	// ← キーの入力フラグ
+// ...
+
+const  MOVE = 5;	//	移動量
+
+
+// 宣言
 var player = function(posx, posy, canvas) {
-    this.posx         = posx;
+    this.posx         = posx;	
     this.posy         = posy;
     this.canvas       = canvas;
 }
+// ...
 
-//MAIN
+
+// MAIN
 $(function(){
-	player1 = new player(250, 250, $('#player1').get(0));
-	player2 = new player(250, 250, $('#player2').get(0));
+	player1 = new player(250, 250, $('#player1').get(0));	//	プレイヤー１の定義
+	player2 = new player(250, 250, $('#player2').get(0));	//	プレイヤー２の定義
 
-	drawPlayer(player1.posx, player1.posy, player1.canvas);
-	drawPlayer(player2.posx, player2.posy, player2.canvas);
+	// drawPlayer(player1.posx, player1.posy, player1.canvas);
+	// drawPlayer(player2.posx, player2.posy, player2.canvas);
+
+	mainloop();
 	Controler();
 });
 
 
+// メインループを定義
+var mainloop = function() {
+
+	//　1フレーム開始時の時間をとる
+	if (count == 0) { set_time = new Date(); }
+
+	//	フレーム数を増やす
+	count++;
+
+    // 処理開始時間を保存
+    var startTime = new Date();
+
+    //　移動処理
+    move();
+
+    // 描画処理
+    redraw();
+
+    // 処理経過時間の保存
+    var deltaTime = (new Date()) - startTime;
+
+    //　次のフレームの間隔
+    // 1フレームにかかる時間 - 1処理にかかった時間
+    var interval = MSPF - deltaTime;
+
+    // 1フレームの時間が残っていたら
+    if(interval > 0) 
+    {
+        // 処理が早すぎるので次のループまで少し待つ
+        setTimeout(mainloop, interval);
+    } 
+    else 
+    {
+        // 処理が遅すぎるので即次のループを実行する
+        // mainloop();
+
+        // 処理が遅すぎるので、次のフレームを休止させる
+        // intarvalは1フレームにかかる時間を超えてしまった分の数(マイナス)
+        setTimeout(mainloop, MSPF + interval);
+        // count++;
+    }
+
+};
+
+
+//	描画関数を定義
+function redraw() {
+
+	// プレイヤー１の描画？
+	drawPlayer(player1.posx, player1.posy, player1.canvas);
+
+	// プレイヤー2の描画？
+	drawPlayer(player2.posx, player2.posy, player2.canvas);
+
+	end_time = new Date();
+
+	//	1秒(1000ミリ秒)経過したら
+	if (end_time - set_time >= 1000)
+	 {
+		//console.log("end_time = " + end_time);
+		//console.log("set_time = " + set_time);
+		//console.log(end_time - set_time);
+
+		console.log(count);	// フレーム数の表示
+		count = 0;			//　フレーム数のリセット
+		set_time = end_time // １フレーム目の時間のセット
+	}
+
+};
+
+
+//	わかんないです
 function drawPlayer(posx, posy, canvas){
 	if(!canvas || !canvas.getContext){
 		console.log("false");
@@ -27,28 +132,116 @@ function drawPlayer(posx, posy, canvas){
 	ctxCanvas.beginPath();
 	ctxCanvas.arc(posx, posy, 20, 0, Math.PI*2, false);
 	ctxCanvas.stroke();
+	//console.log(posx);
+	//console.log(posy);
 };
 
-
+/*
 function Controler(){
 	$(window).keyup(function(e){
 		//左が押されたとき
 		if(e.keyCode == 37 && player1.posx > 40){
-				player1.posx -= 45;
+				player1.posx -= 30;
 		}
 		//右が押されたとき
 		if(e.keyCode == 39 && player1.posx < 460){
-			player1.posx += 45;
+			player1.posx += 30;
 		}
 		//上が押されたとき
 		if(e.keyCode == 38 && player1.posy > 40){
-			player1.posy -= 45;
+			player1.posy -= 30;
 		}
 		//下が押されたとき
 		if(e.keyCode == 40 && player1.posy < 460){
-			player1.posy += 45;
+			player1.posy += 30;
 		}
 		//描画
-		drawPlayer(player1.posx, player1.posy, player1.canvas);
+		//drawPlayer(player1.posx, player1.posy, player1.canvas);
 	});
+};
+*/
+
+function Controler(){
+
+	//	キーが押されたとき？
+	$(window).keydown(function(e) {
+
+		//　← キーが押されたとき
+		if (e.keyCode == 37) {
+			left_flag = 1;	// ← キーフラグをオン	
+		}
+
+		//　→ キーが押されたとき
+		if (e.keyCode == 39) {
+			right_flag = 1;	//　→　キーフラグをオン
+		}
+
+		//　↑ キーが押されたとき
+		if (e.keyCode == 38) {
+			up_flag = 1;	// ↑ キーフラグをオン
+		}
+
+		//　↓ キーが押されたとき
+		if (e.keyCode == 40) {
+			down_flag = 1;	//　↓ キーフラグをオン
+		}
+
+		//描画
+		//drawPlayer(player1.posx, player1.posy, player1.canvas);
+	});
+
+	// キーが離されたとき？
+	$(window).keyup(function(e){
+
+		//　← キーが押されたとき
+		if (e.keyCode == 37) {
+			left_flag = 0;	// ← キーフラグをオフ
+		}
+
+		//　→ キーが押されたとき
+		if (e.keyCode == 39) {
+			right_flag = 0; //　→　キーフラグをオフ
+		}
+
+		//　↑ キーが押されたとき
+		if (e.keyCode == 38) {
+			up_flag = 0;	// ↑ キーフラグをオフ
+		}
+
+		//　↓ キーが押されたとき
+		if (e.keyCode == 40) {
+			down_flag = 0;	//　↓ キーフラグをオフ
+		}
+
+		//描画
+		//drawPlayer(player1.posx, player1.posy, player1.canvas);
+	});
+
+};
+
+//	移動処理
+function move() {
+
+	/*
+	*	更新できる場所であるか
+	*	フラグがオンになっているか
+	*	MOVE ... 移動量
+	*/
+	
+	if (left_flag == 1 && player1.posx > 40) {
+		 player1.posx -= MOVE;
+	}
+
+	if (right_flag == 1 && player1.posx < 460) {
+		player1.posx += MOVE;
+	}
+
+	if (up_flag == 1 && player1.posy > 40) {
+		player1.posy -= MOVE;
+	}
+
+	if (down_flag == 1 && player1.posy < 460) {
+		player1.posy += MOVE;
+	}
+
 };
