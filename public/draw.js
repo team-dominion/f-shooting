46ed3b81332bb3cd39e499dcfaf25e097f9b4f16
daisyo <ngdsyma1@gmail.com>
2015-04-01 21:03:08 +0900

    make

--- public/draw.js
+++ public/draw.js
@@ -1,347 +1,282 @@
-//***************************************
-//
-// Fugishige_shooting
-// http://team-dominion.com/
-//
-//***************************************
+/*
+*
+* Fugishige_shooting
+* http://team-dominioon.com
+*
+*/
 
-/* player */
-var MOVE = 25;
+/* 定数 */
+var RADIUS = 20;
+var SPEED  = 10;
+var EXTRA  = SPEED / 2;
+var FPS = 30;
+var MFPS = 1000 / FPS;
 
-/* bullet */
-var bullet_number = 5;
-var bullet_speed  = 10;
-var big_flag      = 0; // 0 X待機中  1 溜めカウント中  2 発射可能  
-var big_count     = 0;
-var charge_number = 3;
-var charge_time   = 0;
-
-/* fps-parameter */
-var FPS    = 30;
-var MSPF   = 1000 / FPS;
-var count  = 0;
-
-var set_time;
-var end_time;
-var start_time;
-var delta_time;
-var interval;
-
-/* input_key_flag */
-var up_flag       = false;
-var down_flag     = false;
-var right_flag    = false;
-var left_flag     = false;
-var charge_flag   = false;
-
-/* definition */
-player = function(posx, posy, userid, hostid, state) {
-    this.posx   = posx;
-    this.posy   = posy;
-    this.userid = userid;
-    this.hostid = hostid;
-    this.state  = state;  //wait, host, join, play, dead
-}
-bullet = function(posx, posy,bullet_flag) {
+/*
+* direction
+* 方向キーの押下の取得
+* 　　index
+*  　　 0 : key_up
+*   　　1 : key_right
+*   　　2 : key_down
+*   　　3 : key_left
+*   state
+*     true  : onKeyDown
+*     false : onKeyUp
+*/
+var direction = [false, false, false, false];
+/*
+* keys
+* キーの押下の取得
+* 　　index
+*  　　 0 : z_key
+*   　　1 : x_key
+*   　　2 : c_key
+*   state
+*     true  : onKeyDown
+*     false : onKeyUp
+*/
+var keys = [false, false, false];
+/*
+* プレイヤー
+*   posx   : x座標
+*   posy   : y座標
+*   userid :
+*   hostid :
+*   state  :
+*/
+PlayerState = function(posx, posy, userid, hostid, state) {
   this.posx = posx;
   this.posy = posy;
-  this.bullet_flag = bullet_flag;
-}
-
-/*=============================================================================================*/
+  this.userid = userid;
+  this.hostid = hostid;
+  /*
+  * state
+  *   wait :
+  *   host :
+  *   join :
+  *   play :
+  *   dead :
+  */
+  this.state = state;
+};
+/*
+* プレイヤーの状態
+*   positionX : x座標初期位置
+*   positionY : y座標初期位置
+*   velocityX : xの初速度
+*   velocityY : yの初速度
+*   radius    : 半径
+*/
+Player = function(positionX, positionY, velocityX, velocityY, radius) {
+  this.positionX = positionX;
+  this.positionY = positionY;
+  this.velocityX = velocityX;
+  this.velocityY = velocityY;
+  this.radius    = radius;
+};
+/* Main */
 $(function(){
-  /* canvas */
-  playGround = $("#play-ground").get(0);
-  ctxCanvas  = playGround.getContext("2d");
+  /* キャンバス */
+  playGround = $('#play-ground').get(0);
+  ctxCanvas  = playGround.getContext('2d');
 
-  player1 = new player(250, 250);
-  player2 = new player(750, 250);
+  /* プレイヤーのインスタンス */
+  player1 = new PlayerState((playGround.width - playGround.width / 2) / 2, playGround.height / 2);
+  player2 = new PlayerState((playGround.width + playGround.width / 2) / 2, playGround. height / 2);
 
-  for (i = 0; i < bullet_number; i++) {
-    bullet[i] = new bullet(0, 0,false);
-  };
-  big_bullet = new bullet(0,0,false);
+  first_player = new Player(player1.posx, player1.posy, 0, 0, RADIUS);
+  second_player = new Player(player2.posx, player2.posy, 0, 0, RADIUS);
 
+  /* */
   Connect();
-  mainloop();
-  Controler();
+  mainLoop();
+  controler();
 });
 
-/*=============================================================================================*/
-var mainloop = function() {
+function mainLoop() {
+  var start_time = new Date();
 
-  /* frame_config */
-  if (count === 0) {
-    set_time = new Date(); 
-  }
-
-  start_time = new Date();
-
-  /* frame_start */
   update();
   draw();
-  sendPosition();
-  /* frame_end */
 
-  delta_time = (new Date()) - start_time;
-  interval = MSPF - delta_time;
-
-
-  // 1フレームの時間が残っていたら
+  var delt_time = (new Date()) - start_time;
+  var interval = MFPS - delt_time;
   if (interval > 0) {
-    setTimeout(mainloop, interval);
-  } else {
-    setTimeout(mainloop, MSPF + interval);
+    setTimeout(mainLoop, interval);
   }
-
-};
-
-/*=============================================================================================*/
-function draw() {
-  /* reset */
-  ctxCanvas.clearRect(0,0,playGround.width,playGround.height);
-
-  /* draw text */
-  ctxCanvas.font = "20px Arial";
-  ctxCanvas.fillText("CHARGE " + charge_number, 5, 25);
-
-  /* path */
-  ctxCanvas.moveTo(500, 0);
-  ctxCanvas.lineTo(500, 500);
-  ctxCanvas.stroke();
-
-  /* draw player */
-  drawPlayer(player1.posx, player1.posy);
-  drawPlayer(player2.posx, player2.posy);
-
-  /* bullet */
-  for ( i = 0; i < bullet_number; i++) {
-    if (bullet[i].bullet_flag) { 
-      drawBullet(bullet[i].posx, bullet[i].posy)
-    };
-    if (big_bullet.bullet_flag) {
-      drawBigBullet(big_bullet.posx,big_bullet.posy);
-    }
+  else {
+    setTimeout(mainLoop, 0);
   }
-
-  end_time = new Date();
 };
 
-function drawBullet(posx, posy) {
-  if (!playGround || !playGround.getContext) {
-    console.log("drawBullet_false");
-    return false;
-  };
-
-  ctxCanvas.beginPath();
-  ctxCanvas.arc(posx, posy, 10, 0, Math.PI * 2, false);
-  ctxCanvas.stroke();
-};
-
-function drawBigBullet(posx, posy){
-  if (!playGround || !playGround.getContext) {
-    console.log("drawBullet_false");
-    return false;
-  };
-  ctxCanvas.beginPath();
-  ctxCanvas.arc(posx,posy,40,0,Math.PI * 2, false);
-  ctxCanvas.stroke();
-}
-
-function drawPlayer(posx, posy){
-  if (!playGround || !playGround.getContext) {
-    console.log("drawPlayer_false");
-    return false;
-  };
-
-  ctxCanvas.beginPath();
-  ctxCanvas.arc(posx, posy, 20, 0, Math.PI*2, false);
-  ctxCanvas.stroke();
-};
-
-/*=============================================================================================*/
-function Controler(){
-
+function controler() {
+  /* キーを押したかの判定 */
   onKeyDown = function(e) {
+    switch(e.keyCode) {
+      case 37: // 右
+        direction[3] = true;
+        break;
+      case 38: // 左
+        direction[0] = true;
+        break;
+      case 39: // 上
+        direction[1] = true;
+        break;
+      case 40: // 下
+        direction[2] = true;
+        break;
 
-    // ← キー
-    if (e.keyCode === 37 && !left_flag) {
-      left_flag = true;
-    }
-
-    // → キー
-    if (e.keyCode === 39 && !right_flag) {
-      right_flag = true;
-    }
-
-    // ↑ キー
-    if (e.keyCode === 38 && !up_flag) {
-      up_flag = true;
-    }
-
-    // ↓ キー
-    if (e.keyCode === 40 && !down_flag) {
-      down_flag = true;
-    }
-
-    if (!charge_flag) {
-      //c キー
-      if (e.keyCode === 67) {
-        big_flag = 0;
-        charge_flag = true;
-        charge_number++;
-        console.log(charge_flag);
-      }
-      if (e.keyCode === 88 && big_flag === 0) {
-        big_flag = 1;
-      }
-
-      /* 弾が装填されていて、Zが押されたとき*/
-      if (e.keyCode === 90 && charge_number > 0) {
-        big_flag = 0;
-        for (i=0;i<bullet_number;i++){
-          console.log("bullet_"+i);
-
-          if (!bullet[i].bullet_flag) {
-            bullet[i].bullet_flag = true;
-            console.log("shot");
-
-            /* 発射位置取得 */
-            bullet[i].posx = player1.posx;
-            bullet[i].posy = player1.posy;
-            charge_number--;
-
-            break;
-          }
-        }
-      }
-
+      case 67: // c
+        keys[2] = true;
+        break;
+      case 88: // x
+        keys[1] = true;
+        break;
+      case 90: // z
+        keys[0] = true;
+        break;
     }
   };
+  /* キーを離したかの判定 */
+  onKeyUp = function(e) {
+    switch(e.keyCode) {
+      case 37: // 左
+        direction[3] = false;
+        break;
+      case 38: // 上
+        direction[0] = false;
+        break;
+      case 39: // 右
+        direction[1] = false;
+        break;
+      case 40: // 下
+        direction[2] = false;
+        break;
 
-  onKeyUp = function(e){
-
-    // ← キー
-    if (e.keyCode === 37) {
-      left_flag = false;  
-    }
-
-    // → キー
-    if (e.keyCode === 39) {
-      right_flag = false; 
-    }
-
-    // ↑ キー
-    if (e.keyCode === 38) {
-      up_flag = false;  
-    }
-
-    // ↓ キー
-    if (e.keyCode === 40) {
-      down_flag = false;  
-    }
-
-    if (e.keyCode === 88) {
-      console.log("big flag"+big_flag);
-
-      if (big_flag === 2 && charge_number >= 2){
-        charge_number -= 2;
-        big_bullet.bullet_flag = true;
-        big_bullet.posx = player1.posx;
-        big_bullet.posy = player1.posy;
-      } else if (big_flag === 1)  {
-        console.log(big_count);
-      }
-
-      big_flag = 0;
+      case 67: // c
+        keys[2] = false;
+        break;
+      case 88: // x
+        keys[1] = false;
+        break;
+      case 90: // z
+        keys[0] = false;
+        break;
     }
   };
-
-  document.addEventListener("keydown", onKeyDown, false);
-  document.addEventListener("keyup", onKeyUp, false);
-};
-
-/*=============================================================================================*/
+  document.addEventListener('keydown', onKeyDown, false);
+  document.addEventListener('keyup', onKeyUp, false);  
+}
+/*=== update =============================================================================*/
 function update() {
-  Count();
-  /* プレイヤーの更新 */
-  Count();
-  if(!charge_flag){
-    if (left_flag && player1.posx > 40) {
-      player1.posx -= MOVE;
-    }
+  /* スピードテスト（デバッグ）用 */
+  first_player.velocityX = 0;
+  first_player.velocityY = 0;
 
-    if (right_flag && player1.posx < 460) {
-      player1.posx += MOVE;
-    }
-
-    if (up_flag && player1.posy > 40) {
-      player1.posy -= MOVE;
-    }
-
-    if (down_flag && player1.posy < 460) {
-      player1.posy += MOVE;
-    }
-  }
-
-  /* 弾の更新 */
-  for(i=0;i<bullet_number;i++){
-    if (bullet[i].bullet_flag && bullet[i].posx <= 1000) {
-      bullet[i].posx += bullet_speed * 10;
-    } else {
-      bullet[i].bullet_flag = false;
-    }
-  }
-  if(big_bullet.bullet_flag &&big_bullet.posx <= 1000) {
-    big_bullet.posx += bullet_speed * 10;
-  }else{
-    big_bullet.bullet_flag = false;
-  }
-
+  updateFirstPlayer();
+  /* 更新 */
+  updatePlayerConection();
 };
 
-/*=============================================================================================*/
+function updateFirstPlayer() {
+  updateFirstPlayerSpeed();
+
+  first_player.positionX += first_player.velocityX;
+  first_player.positionY += first_player.velocityY;
+
+  if (first_player.positionX - first_player.radius < 0) {
+    first_player.positionX = first_player.radius;
+  }
+  else if (first_player.positionX + first_player.radius > playGround.width / 2) {
+    first_player.positionX = (playGround.width / 2) - first_player.radius;
+  }
+  if (first_player.positionY - first_player.radius < 0) {
+    first_player.positionY = first_player.radius;
+  }
+  else if (first_player.positionY + first_player.radius > playGround.width / 2) {
+    first_player.positionY = playGround.height - first_player.radius;
+  }
+};
+
+function updateFirstPlayerSpeed() {
+  for (var i = 0; i < 4; i++) {
+    if (direction[i] == true) {
+      switch(i) {
+        case 0:
+          first_player.velocityY = -SPEED;
+          break;
+        case 2:
+          first_player.velocityY =  SPEED;
+          break;
+        case 3:
+          first_player.velocityX = -SPEED;
+          break;
+        case 1:
+          first_player.velocityX =  SPEED;
+          break;
+      }
+    }
+  }
+};
+
+function updatePlayerConection() {
+  player1.posx = first_player.positionX;
+  player1.posy = first_player.positionY;
+  player2.posx = second_player.positionX;
+  player2.posy = second_player.positionY;
+};
+/*=== update_end =========================================================================*/
+/*=== draw ===============================================================================*/
+function draw() {
+  /* リセット */
+  ctxCanvas.clearRect(0, 0, playGround.width, playGround.height);
+
+  /* 画面の準備 */
+  ctxCanvas.beginPath();
+  ctxCanvas.moveTo(playGround.width / 2, 0);
+  ctxCanvas.lineTo(playGround.width / 2, playGround.height);
+  ctxCanvas.font = '20px Arial';
+  ctxCanvas.fillText('TEST', 5, 25);
+  ctxCanvas.stroke();
+
+  /* */
+  drawPlayer(first_player.positionX, first_player.positionY);
+  drawPlayer(second_player.positionX, second_player.positionY);
+};
+
+function drawPlayer(posx, posy) {
+  if (!playGround || !playGround.getContext) {
+    console.log('drawPlayer_failed');
+    return false;
+  };
+  ctxCanvas.beginPath();
+  ctxCanvas.arc(posx, posy, RADIUS, 0, Math.PI * 2, false);
+  ctxCanvas.stroke();
+};
+/*=== draw_end ===========================================================================*/
 function Connect(){
 
-  if (typeof io === "undefined") {
-    console.log("connetion failed");
+  if (typeof io === 'undefined') {
+    console.log('connetion failed');
     return;
   }
 
   var socket = io.connect();
   player1.userid = socket.id;
-  player1.state = "wait";
+  player1.state = 'wait';
 
   sendPosition = function(){
-    socket.emit("send_position", player1);
-  }
+    socket.emit('send_position', player1);
+  };
 
-  socket.on("connect", function() {
-    socket.on("receive_position", function(data){
+  socket.on('connect', function() {
+    socket.on('receive_position', function(data){
       player2.posx = data.posx + 500;
       player2.posy = data.posy;
-      $("#position").text("POSX: " + player2.posx + ", POSY: " + player2.posy);
+      $('#position').text('POSX: ' + player2.posx + ', POSY: ' + player2.posy);
     });
 
-    $("#userid").text(player1.userid);
+    $('#userid').text(player1.userid);
   });
 };
-
-function Count(){
-
-  if (charge_flag) {
-    charge_time++;
-  if (charge_time >= 15) {
-    charge_time = 0;
-    charge_flag = false;
-  }
-}
-  if (big_flag === 1 && big_count < 60) {
-    big_count++;
-    if (big_count === 59) {
-      big_flag = 2;
-    }
-  } else {
-    big_count = 0;
-  }
-}
